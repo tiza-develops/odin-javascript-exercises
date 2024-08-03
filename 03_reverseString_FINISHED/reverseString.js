30b2f84103d56704fc48aae068667474fe7130cb
Tiza <tonatiuhcarrillob@tuta.io>
2024-08-03 09:22:12 -0600

    Third exercise completed

---
+++ 03_reverseString_FINISHED/reverseString.js
@@ -0,0 +1,9 @@
+const reverseString = function(string) {
+  let array = [...string]
+  let yarra = array.reverse();
+  let gnirts = yarra.join("");
+  return gnirts;
+};
+
+// Do not edit below this line
+module.exports = reverseString;
