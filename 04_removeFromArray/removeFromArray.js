99f90be87708dacb2d095447cf83e971932c9ffc
Tiza <tonatiuhcarrillob@tuta.io>
2024-08-03 12:12:11 -0600

    4.2 test cleared

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elements) {
+  for (let element of elements) {
+    if (array.includes(element)) {
+      let index = array.indexOf(element);
+      array.splice(index,1);
+    } else {
+      continue;
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
