deecbd38b0ec0ff99f3c2224586f5287bb0ab62a
Tiza <tonatiuhcarrillob@tuta.io>
2024-08-03 12:22:40 -0600

    Fourth exercise completed

--- 04_removeFromArray_FINISHED/removeFromArray.js
+++ 04_removeFromArray_FINISHED/removeFromArray.js
@@ -1,14 +1,15 @@
 const removeFromArray = function(array, ...elements) {
   for (let element of elements) {
-    if (array.includes(element)) {
+    while (array.includes(element)) {
       let index = array.indexOf(element);
       array.splice(index,1);
-    } else {
-      continue;
     }
   }
   return array;
 };
+let array = [1,2,2,2,3];
+removeFromArray(array,2);
+console.log(array);
 
 // Do not edit below this line
 module.exports = removeFromArray;
