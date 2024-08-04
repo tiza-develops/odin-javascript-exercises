4dd6f0f65f5e2062df294b1a4aafc010458f0c02
Tiza <tonatiuhcarrillob@tuta.io>
2024-08-04 06:34:56 -0600

    Revert "Fourth exercise completed"
    This reverts commit deecbd38b0ec0ff99f3c2224586f5287bb0ab62a.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,15 +1,14 @@
 const removeFromArray = function(array, ...elements) {
   for (let element of elements) {
-    while (array.includes(element)) {
+    if (array.includes(element)) {
       let index = array.indexOf(element);
       array.splice(index,1);
+    } else {
+      continue;
     }
   }
   return array;
 };
-let array = [1,2,2,2,3];
-removeFromArray(array,2);
-console.log(array);
 
 // Do not edit below this line
 module.exports = removeFromArray;
