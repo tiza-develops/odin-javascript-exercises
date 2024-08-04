e6edf7b30fac41fca08a9f3039e5efded796e56b
Tiza <tonatiuhcarrillob@tuta.io>
2024-08-04 06:36:43 -0600

    Finally each of the functions necessary for sum(range()) are complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,5 +2,23 @@ const sumAll = function() {
 
 };
 
+function range(num1,num2) {
+  let array = [];
+  for (let i = num1; i <= num2; ++i) {
+    array.push(i);
+  }
+  return array;
+};
+
+function sum(array) {
+  let total = 0;
+  for (const element of array) {
+    total += element;
+  }
+  return total;
+}
+
+console.log(sum(range(1,5)));
+
 // Do not edit below this line
 module.exports = sumAll;
