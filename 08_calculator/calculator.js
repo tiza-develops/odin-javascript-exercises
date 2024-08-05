722a90f2f4a14c15a88e78febe7f54835d4c10c2
Tiza <tonatiuhcarrillob@tuta.io>
2024-08-05 10:36:18 -0600

    sum implemented with reduce() method

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -2,13 +2,17 @@ const add = function(summand1, summand2) {
 	return summand1 + summand2;
 };
 
-const subtract = function() {
-	
+const subtract = function(minuend, substrahend) {
+  return minuend - substrahend;
 };
 
-const sum = function() {
-	
+const sum = function(summands) {
+  const result = summands.reduce((total,currentItem) => {
+    total + currentItem;
+  },0);
+  return result;
 };
+
 
 const multiply = function() {
 
